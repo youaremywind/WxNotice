fad190bea12daa4aaa569828732abe5c59ad8876
kingsley <erkuner@icloud.com>
2022-08-22 15:30:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
 data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
-res = wm.send_template(user_id2, template_id, data)
-print(res)
+user_id = user_id.split(',')
+for userid in user_id:
+    res = wm.send_template(userid, template_id, data)
+    print(res)
